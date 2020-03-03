61e077fa2586d2b614abbd38700fce0a8f9597d6
Alvarlegur <halldoralvar@gmail.com>
2020-03-03 20:54:07 +0000

    sma fix

--- template/src/components/App.js
+++ template/src/components/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import {Switch, Route, Link } from 'react-router-dom';
+import {Switch, Route, Link, NavLink } from 'react-router-dom';
+//import Bubbles from './bubbles/index.js';
+const bubbleService = require('../../../server/services/bubbleService.js');
 
 const App = () => {
+  let test = bubbleService.getProducts();
+
+  console.log(bubbleService.getProducts()); //this is all the bubbles
+  console.log(bubbleService.getBundles()); // these are all the bundles
+  test.forEach(e => console.log(e.name));
     return (
       <Switch>
 
         //testa hvernig route virkar
         <Route exact path='/' render={() => (
-          <Link to='/Bubbles'>Go to Bubbles page </Link>
+          <Link to='/Bubbles/1'>Go to Bubbles page </Link>
         )} />
         <Route exact path='/Bubbles' render={() => (<p>Welcome</p>)} />
         <Route path='/Bubbles/1' render={() => (<p>Welcome to bubbles page 1</p>)} />
--- template/src/components/bubbles/index.js
+++ template/src/components/bubbles/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+const bubbleService = require('../../../server/services/bubbleService.js');
 
 // þetta virkar ekki, var að prófa mig áfram :)
 
-const Bubbles = ({ }) => {
-  return (
-    <View >
-      <Text> Hello im here </Text>
-    </View>
-  );
-};
+class Bubbles extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      bubbleID: "",
+      bubbles: [],
+      bubbleDetails: "",
+    }
+  }
+
+  render(){
+
+    return (<div> Hello guys </div>)
+  }
+}
 
 export default Bubbles;
