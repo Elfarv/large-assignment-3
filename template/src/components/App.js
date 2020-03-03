8055702cfd3c11fe8bd8427e9db8a37e382bf18e
Alvarlegur <halldoralvar@gmail.com>
2020-03-03 19:47:59 +0000

    test með link og route

--- template/src/components/App.js
+++ template/src/components/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import {Switch, Route, Link } from 'react-router-dom';
 
 const App = () => {
-    return <p>Start working here!</p>
+    return (
+      <Switch>
+
+        //testa hvernig route virkar
+        <Route exact path='/' render={() => (
+          <Link to='/Bubbles'>Go to Bubbles page </Link>
+        )} />
+        <Route exact path='/Bubbles' render={() => (<p>Welcome</p>)} />
+        <Route path='/Bubbles/1' render={() => (<p>Welcome to bubbles page 1</p>)} />
+        <Route render={( {location}) => (
+          <div>
+            <div>404!</div>
+            <div> {location.pathname} was not found! </div>
+          </div>
+        )} />
+      </Switch>
+    )
 };
 
 export default App;
---
+++ template/src/components/bubbles/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+// þetta virkar ekki, var að prófa mig áfram :)
+
+const Bubbles = ({ }) => {
+  return (
+    <View >
+      <Text> Hello im here </Text>
+    </View>
+  );
+};
+
+export default Bubbles;
